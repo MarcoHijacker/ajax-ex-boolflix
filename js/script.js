f5a50ba81125eee6cedfef32bfc86104e843be0d
Marco <marco.longo@hotmail.it>
2020-07-27 15:51:28 +0200

    Milestone 1 completata.

--- js/script.js
+++ js/script.js
@@ -14,9 +14,62 @@
 // Area init
 
 function init() {
-
+  addSearchBtnListener();
 }
 
 $(document).ready(init);
 
 // Area funzioni
+
+function addSearchBtnListener() {
+  $('#search-btn').click(performSearch);
+}
+
+function performSearch() {
+  var inputValue = $('#film-search').val();
+
+  $.ajax({
+    url: 'https://api.themoviedb.org/3/search/movie',
+    method: 'GET',
+    data: {
+      'api_key': 'd6c46515c238258071e0cac478212d4b',
+      'query': inputValue,
+      'language': 'it-IT'
+    },
+    success: function(data) {
+      var success = data['success'];
+      var dataResults = data['results'];
+      var numResults = data['total_results'];
+
+      var template = $('#film-template').html();
+      var compiled = Handlebars.compile(template);
+      var target = $('#results-list');
+
+      target.html('');
+
+      console.log('target');
+
+      if(success != false) {
+        for (var i = 0; i < dataResults.length; i++) {
+          var filmHTML = compiled({
+            'filmId': i,
+            'filmTitle': dataResults[i].title,
+            'origTitle': dataResults[i].original_title,
+            'filmLanguage': dataResults[i].original_language,
+            'filmRate': dataResults[i].vote_average
+          });
+          target.append(filmHTML);
+        }
+
+      } else {
+        console.log('error');
+      }
+    },
+    error: function(request, state, error) {
+      console.log('request', request);
+      console.log('state', state);
+      console.log('error', error);
+    }
+  });
+
+}
