e179ae07591e7914390fe163dd4737c6b0ad24b1
Marco <marco.longo@hotmail.it>
2020-07-29 12:37:57 +0200

    Milestone 2 completata (JS fix).

--- js/script.js
+++ js/script.js
@@ -1,21 +1,7 @@
 // Esercizio Boolflix JS
 
-// Milestone 1: COMPLETA
-// Milestone 2: Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da
-// permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
-// lasciando le restanti vuote (troviamo le icone in FontAwesome).
-// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze
-// piene (o mezze vuote :P)
-// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della
-// nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della
-// nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca
-// dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando
-// attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di
-// risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
-// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=s
-// crubs
+// Milestone 1: COMPLETA!
+// Milestone 2: COMPLETA!
 
 // Area init
 
@@ -28,17 +14,22 @@ $(document).ready(init);
 // Area funzioni
 
 function addSearchBtnListener() {
-  $('#search-btn').click(performSearch);
-  $('#film-search').keydown(function() {
+  $('#search-btn').click(function() {
+    filmSearch();
+    serieSearch();
+  });
+  $('#query-search').keydown(function() {
     var key = event.which;
     if (key == 13) {
-      performSearch();
+      filmSearch();
+      serieSearch();
     }
   })
 }
 
-function performSearch() {
-  var inputValue = $('#film-search').val();
+// Funzione di ricerca film
+function filmSearch() {
+  var inputValue = $('#query-search').val();
 
   // Chiamata AJAX per i film
   $.ajax({
@@ -53,9 +44,6 @@ function performSearch() {
       var success = data['success'];
       var dataResults = data['results'];
       var numResults = data['total_results'];
-      var fullStar = '<i class="fas fa-star"></i>';
-      var emptyStar = '<i class="far fa-star"></i>';
-      var tempStar = '';
 
       var template = $('#film-template').html();
       var compiled = Handlebars.compile(template);
@@ -69,53 +57,22 @@ function performSearch() {
           var origTitle = dataResults[i].original_title;
           var filmRate = Math.ceil(dataResults[i].vote_average / 2);
           var filmLanguage = dataResults[i].original_language;
+          var starRate = '';
+          var filmFlag = '';
+
+          starRate = getStarsRate(filmRate);
+          filmFlag = getFlag(filmLanguage);
+
           var filmHTML = compiled({
             'filmId': i,
             'filmTitle': filmTitle,
             'origTitle': origTitle,
-            // 'filmLanguage': filmLanguage,
-            // 'filmRate': filmRate
+            'starRate': starRate,
+            'filmRate': filmRate,
+            'filmFlag': filmFlag
           });
 
           target.append(filmHTML);
-
-          switch (filmRate) {
-            case 1:
-              tempStar= '<span>Voto in stelle: ' + fullStar + emptyStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 2:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 3:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 4:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + fullStar + emptyStar + '</span><br>';
-              break;
-            case 5:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + fullStar + fullStar + '</span><br>';
-              break;
-            default:
-              tempStar= '<span>Voto in stelle: ' + emptyStar + emptyStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-          }
-
-          var starTarget = $('li[data-film="' + i + '"]');
-
-          starTarget.append(tempStar);
-
-          switch (filmLanguage) {
-            case 'it':
-              starTarget.append('<span>Lingua film: <img src="img/italy.png"></span>');
-              break;
-            case 'en':
-              starTarget.append('<span>Lingua film: <img src="img/england.png"></span>');
-              break;
-            case 'ja':
-              starTarget.append('<span>Lingua film: <img src="img/japan.png"></span>');
-              break;
-            default:
-              starTarget.append('<span>Lingua film: <img src="img/unknown.png"></span>');
-          }
 
         }
 
@@ -129,8 +86,13 @@ function performSearch() {
       console.log('error', error);
     }
   });
+}
 
-  // Chiamata AJAX per le serie TV
+// Funzione di ricerca serie TV
+function serieSearch() {
+  var inputValue = $('#query-search').val();
+
+  // Chiamata AJAX per le serie
   $.ajax({
     url: 'https://api.themoviedb.org/3/search/tv',
     method: 'GET',
@@ -143,9 +105,6 @@ function performSearch() {
       var success = data['success'];
       var dataResults = data['results'];
       var numResults = data['total_results'];
-      var fullStar = '<i class="fas fa-star"></i>';
-      var emptyStar = '<i class="far fa-star"></i>';
-      var tempStar = '';
 
       var template = $('#serie-template').html();
       var compiled = Handlebars.compile(template);
@@ -159,54 +118,22 @@ function performSearch() {
           var serieOrigTitle = dataResults[i].original_name;
           var serieRate = Math.ceil(dataResults[i].vote_average / 2);
           var serieLanguage = dataResults[i].original_language;
+          var starRate = '';
+          var serieFlag = '';
+
+          starRate = getStarsRate(serieRate);
+          serieFlag = getFlag(serieLanguage);
+
           var serieHTML = compiled({
             'serieId': i,
             'serieTitle': serieTitle,
             'serieOrigTitle': serieOrigTitle,
-            // 'serieLanguage': serieLanguage,
-            // 'serieRate': serieRate
+            'starRate': starRate,
+            'serieRate': serieRate,
+            'serieFlag': serieFlag
           });
 
           target.append(serieHTML);
-
-          switch (serieRate) {
-            case 1:
-              tempStar= '<span>Voto in stelle: ' + fullStar + emptyStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 2:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 3:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + emptyStar + emptyStar + '</span><br>';
-              break;
-            case 4:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + fullStar + emptyStar + '</span><br>';
-              break;
-            case 5:
-              tempStar= '<span>Voto in stelle: ' + fullStar + fullStar + fullStar + fullStar + fullStar + '</span><br>';
-              break;
-            default:
-              tempStar= '<span>Voto in stelle: ' + emptyStar + emptyStar + emptyStar + emptyStar + emptyStar + '</span><br>';
-          }
-
-          var starTarget = $('li[data-serie="' + i + '"]');
-
-          starTarget.append(tempStar);
-
-          switch (serieLanguage) {
-            case 'it':
-              starTarget.append('<span>Lingua serie: <img src="img/italy.png"></span>');
-              break;
-            case 'en':
-              starTarget.append('<span>Lingua serie: <img src="img/england.png"></span>');
-              break;
-            case 'ja':
-              starTarget.append('<span>Lingua serie: <img src="img/japan.png"></span>');
-              break;
-            default:
-              starTarget.append('<span>Lingua serie: <img src="img/unknown.png"></span>');
-          }
-
         }
 
       } else {
@@ -219,6 +146,42 @@ function performSearch() {
       console.log('error', error);
     }
   });
+  // Elimino l'input solo dopo la fine della funzione serieSearch() che avviene dopo filmSearch()
+  $('#query-search').val('');
+}
 
-  $('#film-search').val('');
+// Dato un valore da 0 a 5 (rate) restituisco una stringa HTML con la valutazione a stelle
+function getStarsRate(rate) {
+  var starRate = '';
+  var fullStar = '<i class="fas fa-star"></i>';
+  var emptyStar = '<i class="far fa-star"></i>';
+
+  for (var j = 0; j < 5; j++) {
+    if(j < rate) {
+      starRate += fullStar;
+    } else {
+      starRate += emptyStar;
+    }
+  }
+  return starRate;
+}
+
+// Data una stringa lingua (it, en, ja), restituisco l'html dell'img relativa alla nazionalità
+function getFlag(language) {
+  var flag = '';
+
+  switch (language) {
+    case 'it':
+    flag = `<img src="img/${language}.png">`;
+    break;
+    case 'en':
+    flag = `<img src="img/${language}.png">`;
+    break;
+    case 'ja':
+    flag = `<img src="img/${language}.png">`;
+    break;
+    default:
+    flag = `<img src="img/unknown.png">`;
+  }
+  return flag;
 }
